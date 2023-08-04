12abcb36cd4ed09c19bbf5545874f5091acdfe03
Kegan Maher <kegan@compiler.la>
2023-08-04 02:01:28 +0000

    refactor(argparser): simplify username arg, cleanup

--- compiler_admin/main.py
+++ compiler_admin/main.py
@@ -21,17 +21,22 @@ def main(argv=None):
 
     subparsers = parser.add_subparsers(dest="command")
 
-    def _subcmd(name, help):
+    def _subcmd(name, help, add_username_arg=False):
+        """Helper creates a new subcommand parser."""
         parser = subparsers.add_parser(name, help=help)
+        if add_username_arg is True:
+            parser.add_argument("username", help="The user's account name, sans domain.")
         return parser
 
     _subcmd("info", help="Print configuration and debugging information.")
 
-    init_parser = _subcmd("init", help="Initialize a new admin project. This command should be run once before any others.")
-    init_parser.add_argument("admin_user")
+    _subcmd(
+        "init",
+        help="Initialize a new admin project. This command should be run once before any others.",
+        add_username_arg=True,
+    )
 
-    create_user_parser = _subcmd("create", help="Create a new user in the Compiler domain.")
-    create_user_parser.add_argument("username")
+    _subcmd("create", help="Create a new user in the Compiler domain.", add_username_arg=True)
 
     if len(argv) == 0:
         argv = ["info"]
@@ -43,7 +48,7 @@ def main(argv=None):
     elif args.command == "create":
         return create(args.username, *extra)
     elif args.command == "init":
-        return init(args.admin_user)
+        return init(args.username)
 
 
 if __name__ == "__main__":
