cb355eb0384a723d9db35f326dc307b745a6f648
Kegan Maher <kegan@compiler.la>
2023-08-15 15:15:03 +0000

    refactor(init): optionally init GAM, GYB projects

--- compiler_admin/commands/init.py
+++ compiler_admin/commands/init.py
@@ -7,44 +7,56 @@ from compiler_admin.commands import RESULT_FAILURE, RESULT_SUCCESS
 from compiler_admin.services.google import USER_ARCHIVE, CallGAMCommand
 
 
-CONFIG_DIR = os.environ.get("GAMCFGDIR", "./.config")
-CONFIG_PATH = Path(CONFIG_DIR)
-CONFIG_PATH_NAME = str(CONFIG_PATH)
+GAM_CONFIG_DIR = os.environ.get("GAMCFGDIR", "./.config/gam")
+GAM_CONFIG_PATH = Path(GAM_CONFIG_DIR)
+GYB_CONFIG_PATH = GAM_CONFIG_PATH.parent / "gyb"
 
 
-def _clean_config_dir():
-    for path in CONFIG_PATH.glob("**/*"):
+def _clean_config_dir(config_dir: Path) -> None:
+    config_dir.mkdir(parents=True, exist_ok=True)
+    for path in config_dir.glob("**/*"):
         if path.is_file():
             path.unlink()
         elif path.is_dir():
             rmtree(path)
 
 
-def init(admin_user: str) -> int:
+def init(admin_user: str, gam: bool = False, gyb: bool = False) -> int:
     """Initialize a new GAM project.
 
     See https://github.com/taers232c/GAMADV-XTD3/wiki/How-to-Install-Advanced-GAM
 
     Args:
         admin_user (str): The Compiler admin with which to initialize a new project.
+
+        gam (bool): If True, initialize a new GAM project.
+
+        gyb (bool): If True, initialize a new GYB project.
+
     Returns:
         A value indicating if the operation succeeded or failed.
     """
-    if CONFIG_PATH.exists():
-        _clean_config_dir()
+    res = RESULT_SUCCESS
 
-    res = CallGAMCommand(("config", "drive_dir", CONFIG_PATH_NAME, "verify"))
-    res += CallGAMCommand(("create", "project"))
-    res += CallGAMCommand(("oauth", "create"))
-    res += CallGAMCommand(("user", admin_user, "check", "serviceaccount"))
+    if gam:
+        _clean_config_dir(GAM_CONFIG_PATH)
+        # GAM is already installed via pyproject.toml
+        res += CallGAMCommand(("config", "drive_dir", str(GAM_CONFIG_PATH), "verify"))
+        res += CallGAMCommand(("create", "project"))
+        res += CallGAMCommand(("oauth", "create"))
+        res += CallGAMCommand(("user", admin_user, "check", "serviceaccount"))
 
-    # download GYB installer to config directory
-    gyb = CONFIG_PATH / "gyb-install.sh"
-    with gyb.open("w+") as dest:
-        res += subprocess.call(("curl", "-s", "-S", "-L", "https://gyb-shortn.jaylee.us/gyb-install"), stdout=dest)
+    if gyb:
+        _clean_config_dir(GYB_CONFIG_PATH)
+        # download GYB installer to config directory
+        gyb = GYB_CONFIG_PATH / "gyb-install.sh"
+        with gyb.open("w+") as dest:
+            res += subprocess.call(("curl", "-s", "-S", "-L", "https://gyb-shortn.jaylee.us/gyb-install"), stdout=dest)
 
-    # install, giving values to options that prompt by default
-    # https://github.com/GAM-team/got-your-back/blob/main/install-gyb.sh
-    res += subprocess.call((gyb, "-u", admin_user, "-r", USER_ARCHIVE))
+        # install, giving values to some options
+        # https://github.com/GAM-team/got-your-back/blob/main/install-gyb.sh
+        #
+        # use GYB_CONFIG_PATH.parent for the install directory option, otherwise we get a .config/gyb/gyb directory structure
+        res += subprocess.call((gyb, "-u", admin_user, "-r", USER_ARCHIVE, "-d", str(GYB_CONFIG_PATH.parent)))
 
     return RESULT_SUCCESS if res == RESULT_SUCCESS else RESULT_FAILURE
--- tests/commands/test_init.py
+++ tests/commands/test_init.py
@@ -1,11 +1,21 @@
 import pytest
 
-from compiler_admin.commands.init import init, __name__ as MODULE
+from compiler_admin.commands.init import _clean_config_dir, init, __name__ as MODULE
 
 
 @pytest.fixture
-def mock_CONFIG_PATH(mocker):
-    return mocker.patch(f"{MODULE}.CONFIG_PATH")
+def mock_rmtree(mocker):
+    return mocker.patch(f"{MODULE}.rmtree")
+
+
+@pytest.fixture
+def mock_GAM_CONFIG_PATH(mocker):
+    return mocker.patch(f"{MODULE}.GAM_CONFIG_PATH")
+
+
+@pytest.fixture
+def mock_GYB_CONFIG_PATH(mocker):
+    return mocker.patch(f"{MODULE}.GYB_CONFIG_PATH")
 
 
 @pytest.fixture
@@ -23,23 +33,43 @@ def mock_subprocess_call(mocker):
     return mocker.patch(f"{MODULE}.subprocess.call")
 
 
-def test_init_config_path_exists(mock_CONFIG_PATH, mock_clean_config_dir, mock_google_CallGAMCommand, mock_subprocess_call):
-    mock_CONFIG_PATH.exists.return_value = True
+def test_clean_config_dir(mocker, mock_GAM_CONFIG_PATH, mock_rmtree):
+    mock_file = mocker.Mock(is_file=mocker.Mock(return_value=True))
+    mock_dir = mocker.Mock(is_file=mocker.Mock(return_value=False), is_dir=mocker.Mock(return_value=True))
 
-    init("username")
+    mock_GAM_CONFIG_PATH.glob.return_value = [mock_file, mock_dir]
 
-    mock_clean_config_dir.assert_called_once()
-    assert mock_google_CallGAMCommand.call_count > 0
-    assert mock_subprocess_call.call_count > 0
+    _clean_config_dir(mock_GAM_CONFIG_PATH)
+
+    mock_GAM_CONFIG_PATH.mkdir.assert_called_once()
+    mock_GAM_CONFIG_PATH.glob.assert_called_once()
+    mock_file.is_file.assert_called_once()
+    mock_file.unlink.assert_called_once()
+    mock_dir.is_file.assert_called_once()
+    mock_dir.is_dir.assert_called_once()
+    mock_rmtree.assert_called_once()
+    assert mock_dir in mock_rmtree.call_args.args
 
 
-def test_init_config_path_does_not_exist(
-    mock_CONFIG_PATH, mock_clean_config_dir, mock_google_CallGAMCommand, mock_subprocess_call
-):
-    mock_CONFIG_PATH.exists.return_value = False
-
+def test_init_default(mock_clean_config_dir, mock_google_CallGAMCommand, mock_subprocess_call):
     init("username")
 
     assert mock_clean_config_dir.call_count == 0
+    assert mock_google_CallGAMCommand.call_count == 0
+    assert mock_subprocess_call.call_count == 0
+
+
+def test_init_gam(mock_GAM_CONFIG_PATH, mock_clean_config_dir, mock_google_CallGAMCommand):
+    init("username", gam=True, gyb=False)
+
+    mock_clean_config_dir.assert_called_once()
+    assert mock_GAM_CONFIG_PATH in mock_clean_config_dir.call_args.args
     assert mock_google_CallGAMCommand.call_count > 0
+
+
+def test_init_gyb(mock_GYB_CONFIG_PATH, mock_clean_config_dir, mock_subprocess_call):
+    init("username", gam=False, gyb=True)
+
+    mock_clean_config_dir.assert_called_once()
+    assert mock_GYB_CONFIG_PATH in mock_clean_config_dir.call_args.args
     assert mock_subprocess_call.call_count > 0
