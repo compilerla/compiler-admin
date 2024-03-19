148245a84847026350b58d0279d629b11aa7ea28
Kegan Maher <kegan@compiler.la>
2024-03-19 16:56:25 +0000

    test(commands/create): ensure extra args are passed along

--- tests/commands/test_create.py
+++ tests/commands/test_create.py
@@ -68,3 +68,20 @@ def test_create_user_notify(mock_google_user_exists, mock_google_CallGAMCommand,
     assert f"notify notification@example.com from {USER_HELLO}" in call_args
 
     mock_google_add_user_to_group.assert_called_once()
+
+
+def test_create_user_extras(mock_google_user_exists, mock_google_CallGAMCommand, mock_google_add_user_to_group):
+    mock_google_user_exists.return_value = False
+
+    args = Namespace(username="username")
+    res = create(args, "extra1", "extra2")
+
+    assert res == RESULT_SUCCESS
+
+    mock_google_CallGAMCommand.assert_called_once()
+    call_args = " ".join(mock_google_CallGAMCommand.call_args[0][0])
+    assert "create user" in call_args
+    assert "password random changepassword" in call_args
+    assert "extra1 extra2" in call_args
+
+    mock_google_add_user_to_group.assert_called_once()
