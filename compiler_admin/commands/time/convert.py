8649d059d7eb2ae6c6cba427be545dec9888cfb2
Kegan Maher <kegan@compiler.la>
2024-04-25 15:43:08 +0000

    feat(time): wire up toggl --> harvest converter

--- compiler_admin/commands/time/convert.py
+++ compiler_admin/commands/time/convert.py
@@ -3,14 +3,17 @@ from argparse import Namespace
 import pandas as pd
 
 from compiler_admin import RESULT_SUCCESS
-from compiler_admin.services.toggl import INPUT_COLUMNS, convert_to_harvest
+from compiler_admin.services.harvest import INPUT_COLUMNS as TOGGL_COLUMNS, convert_to_toggl
+from compiler_admin.services.toggl import INPUT_COLUMNS as HARVEST_COLUMNS, convert_to_harvest
 
 
 def _get_source_converter(source):
     columns = pd.read_csv(source, nrows=0).columns.tolist()
 
-    if set(INPUT_COLUMNS) < set(columns):
+    if set(HARVEST_COLUMNS) <= set(columns):
         return convert_to_harvest
+    elif set(TOGGL_COLUMNS) <= set(columns):
+        return convert_to_toggl
     else:
         raise NotImplementedError("A converter for the given source data does not exist.")
 
--- tests/commands/time/test_convert.py
+++ tests/commands/time/test_convert.py
@@ -1,8 +1,15 @@
 from argparse import Namespace
+from io import StringIO
 import pytest
 
 from compiler_admin import RESULT_SUCCESS
-from compiler_admin.commands.time.convert import _get_source_converter, convert_to_harvest, convert, __name__ as MODULE
+from compiler_admin.commands.time.convert import (
+    __name__ as MODULE,
+    _get_source_converter,
+    convert_to_harvest,
+    convert_to_toggl,
+    convert,
+)
 
 
 @pytest.fixture
@@ -10,15 +17,23 @@ def mock_get_source_converter(mocker):
     return mocker.patch(f"{MODULE}._get_source_converter")
 
 
-def test_get_source_converter_match(toggl_file):
+def test_get_source_converter_match_toggl(toggl_file):
     result = _get_source_converter(toggl_file)
 
     assert result == convert_to_harvest
 
 
-def test_get_source_converter_mismatch(harvest_file):
+def test_get_source_converter_match_harvest(harvest_file):
+    result = _get_source_converter(harvest_file)
+
+    assert result == convert_to_toggl
+
+
+def test_get_source_converter_mismatch():
+    data = StringIO("one,two,three\n1,2,3")
+
     with pytest.raises(NotImplementedError, match="A converter for the given source data does not exist."):
-        _get_source_converter(harvest_file)
+        _get_source_converter(data)
 
 
 def test_convert(mock_get_source_converter):
