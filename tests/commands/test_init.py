4b56443b4527f464bfd36357bbefeb8d4b6573e6
Kegan Maher <kegan@compiler.la>
2024-12-19 17:37:32 +0000

    refactor(init): describe as @click.command

--- tests/commands/test_init.py
+++ tests/commands/test_init.py
@@ -1,6 +1,6 @@
-from argparse import Namespace
 import pytest
 
+from compiler_admin import RESULT_SUCCESS
 from compiler_admin.commands.init import _clean_config_dir, init, __name__ as MODULE
 
 
@@ -52,35 +52,28 @@ def test_clean_config_dir(mocker, mock_GAM_CONFIG_PATH, mock_rmtree):
     assert mock_dir in mock_rmtree.call_args.args
 
 
-def test_init_admin_user_required():
-    args = Namespace()
+def test_init_default(cli_runner, mock_clean_config_dir, mock_google_CallGAMCommand, mock_subprocess_call):
+    result = cli_runner.invoke(init, ["username"])
 
-    with pytest.raises(ValueError, match="username is required"):
-        init(args)
-
-
-def test_init_default(mock_clean_config_dir, mock_google_CallGAMCommand, mock_subprocess_call):
-    args = Namespace(username="username")
-    init(args)
-
+    assert result.exit_code == RESULT_SUCCESS
     assert mock_clean_config_dir.call_count == 0
     assert mock_google_CallGAMCommand.call_count == 0
     assert mock_subprocess_call.call_count == 0
 
 
-def test_init_gam(mock_GAM_CONFIG_PATH, mock_clean_config_dir, mock_google_CallGAMCommand):
-    args = Namespace(username="username", gam=True, gyb=False)
-    init(args)
+def test_init_gam(cli_runner, mock_GAM_CONFIG_PATH, mock_clean_config_dir, mock_google_CallGAMCommand):
+    result = cli_runner.invoke(init, ["--gam", "username"])
 
+    assert result.exit_code == RESULT_SUCCESS
     mock_clean_config_dir.assert_called_once()
     assert mock_GAM_CONFIG_PATH in mock_clean_config_dir.call_args.args
     assert mock_google_CallGAMCommand.call_count > 0
 
 
-def test_init_gyb(mock_GYB_CONFIG_PATH, mock_clean_config_dir, mock_subprocess_call):
-    args = Namespace(username="username", gam=False, gyb=True)
-    init(args)
+def test_init_gyb(cli_runner, mock_GYB_CONFIG_PATH, mock_clean_config_dir, mock_subprocess_call):
+    result = cli_runner.invoke(init, ["--gyb", "username"])
 
+    assert result.exit_code == RESULT_SUCCESS
     mock_clean_config_dir.assert_called_once()
     assert mock_GYB_CONFIG_PATH in mock_clean_config_dir.call_args.args
     assert mock_subprocess_call.call_count > 0
