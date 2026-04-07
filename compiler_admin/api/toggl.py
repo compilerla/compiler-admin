587b240d20917d410f4bfd42bec6bd0a502c5c70
Kegan Maher <kegan@compiler.la>
2026-04-07 21:20:05 +0000

    refactor(api/toggl): organize with classes

--- compiler_admin/api/toggl.py
+++ compiler_admin/api/toggl.py
@@ -6,39 +6,28 @@ import requests
 from compiler_admin import __version__
 
 
-class Toggl:
-    """Toggl API Client.
+class TogglBase:
+    """Base class for Toggl API clients.
 
     See https://engineering.toggl.com/docs/.
+
+    Sub-classes should implement api_url_resource(self).
     """
 
     API_BASE_URL = "https://api.track.toggl.com"
-    API_REPORTS_BASE_URL = "reports/api/v3"
-    API_VERSION_URL = "api/v9"
-    API_WORKSPACE = "workspace/{}"
-    API_WORKSPACES = "workspaces/{}"
+    API_VERSION = "api/v9"
     API_HEADERS = {"Content-Type": "application/json", "User-Agent": "compilerla/compiler-admin:{}".format(__version__)}
 
     def __init__(self, api_token: str, workspace_id: int, **kwargs):
         self._token = api_token
         self.workspace_id = workspace_id
 
-        self.headers = dict(Toggl.API_HEADERS)
+        self.headers = dict(TogglBase.API_HEADERS)
         self.headers.update(self._authorization_header())
 
         self.session = requests.Session()
         self.session.headers.update(self.headers)
         self.timeout = int(kwargs.get("timeout", 5))
-
-    @property
-    def workspace_url_fragment(self):
-        """The workspace portion of an API URL."""
-        return Toggl.API_WORKSPACE.format(self.workspace_id)
-
-    @property
-    def workspaces_url_fragment(self):
-        """The workspaces portion of an API URL."""
-        return Toggl.API_WORKSPACES.format(self.workspace_id)
 
     def _authorization_header(self):
         """Gets an `Authorization: Basic xyz` header using the Toggl API token.
@@ -49,16 +38,32 @@ class Toggl:
         creds64 = b64encode(bytes(creds, "utf-8")).decode("utf-8")
         return {"Authorization": "Basic {}".format(creds64)}
 
-    def _make_api_url(self, endpoint: str):
-        """Get a fully formed URL for the Toggl API version endpoint."""
-        return "/".join((Toggl.API_BASE_URL, Toggl.API_VERSION_URL, self.workspaces_url_fragment, endpoint))
+    @property
+    def api_url_resource(self):
+        """Sub-classes should implement this prop to use make_api_url() for the given API resource."""
+        raise NotImplementedError("Implement this property to use make_api_url().")
 
-    def _make_report_url(self, endpoint: str):
-        """Get a fully formed URL for the Toggl Reports API v3 endpoint.
+    @property
+    def api_url_version(self):
+        """The version information for an API request URL."""
+        return self.API_VERSION
 
-        See https://engineering.toggl.com/docs/reports_start.
-        """
-        return "/".join((Toggl.API_BASE_URL, Toggl.API_REPORTS_BASE_URL, self.workspace_url_fragment, endpoint))
+    def make_api_url(self, endpoint: str):
+        """Get a fully formed and versioned URL for an endpoint within the Toggl API."""
+        return "/".join((TogglBase.API_BASE_URL, self.api_url_version, self.api_url_resource, endpoint))
+
+
+class TogglReports(TogglBase):
+    REPORTS_API_VERSION = "reports/api/v3"
+    WORKSPACE_ID = "workspace/{}"
+
+    @property
+    def api_url_resource(self):
+        return self.WORKSPACE_ID.format(self.workspace_id)
+
+    @property
+    def api_url_version(self):
+        return self.REPORTS_API_VERSION
 
     def detailed_time_entries(self, start_date: datetime, end_date: datetime, **kwargs) -> requests.Response:
         """Request a CSV report from Toggl of detailed time entries for the given date range.
@@ -113,19 +118,28 @@ class Toggl:
 
         See https://engineering.toggl.com/docs/reports_start.
         """
-        url = self._make_report_url(endpoint)
+        url = self.make_api_url(endpoint)
 
         response = self.session.post(url, json=kwargs, timeout=self.timeout)
         response.raise_for_status()
 
         return response
 
-    def update_workspace_preferences(self, **kwargs) -> requests.Response:
+
+class TogglWorkspace(TogglBase):
+    WORKSPACES_ID = "workspaces/{}"
+
+    @property
+    def api_url_resource(self):
+        """The workspaces portion of an API URL."""
+        return self.WORKSPACES_ID.format(self.workspace_id)
+
+    def update_preferences(self, **kwargs) -> requests.Response:
         """Update workspace preferences.
 
         See https://engineering.toggl.com/docs/api/preferences/#post-update-workspace-preferences.
         """
-        url = self._make_api_url("preferences")
+        url = self.make_api_url("preferences")
 
         response = self.session.post(url, json=kwargs, timeout=self.timeout)
         response.raise_for_status()
--- tests/api/test_toggl.py
+++ tests/api/test_toggl.py
@@ -4,8 +4,7 @@ from pathlib import Path
 import pytest
 
 from compiler_admin import __version__
-from compiler_admin.api.toggl import Toggl
-from compiler_admin.api.toggl import __name__ as MODULE
+from compiler_admin.api.toggl import __name__ as MODULE, TogglBase, TogglReports, TogglWorkspace
 
 
 @pytest.fixture
@@ -13,101 +12,120 @@ def mock_requests(mocker):
     return mocker.patch(f"{MODULE}.requests.Session").return_value
 
 
-@pytest.fixture
-def toggl():
-    return Toggl("token", 1234)
+class TestTogglBase:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.toggl = TogglBase("token", 1234)
+
+    def test_init(self):
+        assert self.toggl.workspace_id == 1234
+
+        token64 = "dG9rZW46YXBpX3Rva2Vu"
+        assert self.toggl._token == "token"
+        assert self.toggl.headers["Authorization"] == f"Basic {token64}"
+
+        assert self.toggl.headers["Content-Type"] == "application/json"
+
+        user_agent = self.toggl.headers["User-Agent"]
+        assert "compilerla/compiler-admin" in user_agent
+        assert __version__ in user_agent
+
+        assert self.toggl.timeout == 5
+
+    def test_api_url_resource(self):
+        with pytest.raises(NotImplementedError):
+            assert self.toggl.api_url_resource
+
+    def test_api_url_version(self):
+        assert self.toggl.api_url_version == self.toggl.API_VERSION
+
+    def test_make_api_url(self):
+        class TogglTest(TogglBase):
+            @property
+            def api_url_resource(self):
+                return "resource"
+
+        self.toggl = TogglTest(self.toggl._token, self.toggl.workspace_id)
+
+        url = self.toggl.make_api_url("endpoint")
+
+        assert url.startswith(self.toggl.API_BASE_URL)
+        assert self.toggl.api_url_version in url
+        assert self.toggl.api_url_resource in url
+        assert "/endpoint" in url
 
 
-@pytest.fixture
-def toggl_mock_post_reports(mocker, toggl, toggl_file):
-    # setup a mock response to a requests.post call
-    mock_csv_bytes = Path(toggl_file).read_bytes()
-    mock_post_response = mocker.Mock()
-    mock_post_response.raise_for_status.return_value = None
-    # prepend the BOM to the mock content
-    mock_post_response.content = b"\xef\xbb\xbf" + mock_csv_bytes
-    # override the requests.post call to return the mock response
-    mocker.patch.object(toggl, "post_reports", return_value=mock_post_response)
-    return toggl
+class TestTogglReports:
+    @pytest.fixture(autouse=True)
+    def setup(self, mock_requests):
+        self.toggl = TogglReports("token", 1234)
+
+    @pytest.fixture
+    def toggl_mock_post_reports(self, mocker, toggl_file):
+        # setup a mock response to a requests.post call
+        mock_csv_bytes = Path(toggl_file).read_bytes()
+        mock_post_response = mocker.Mock()
+        mock_post_response.raise_for_status.return_value = None
+        # prepend the BOM to the mock content
+        mock_post_response.content = b"\xef\xbb\xbf" + mock_csv_bytes
+        # override the requests.post call to return the mock response
+        mocker.patch.object(self.toggl, "post_reports", return_value=mock_post_response)
+        return self.toggl
+
+    def test_api_url_resource(self):
+        assert self.toggl.api_url_resource == "workspace/1234"
+
+    def test_api_url_version(self):
+        assert self.toggl.api_url_version == self.toggl.REPORTS_API_VERSION
+
+    def test_toggl_post_reports(self, mock_requests):
+        url = self.toggl.make_api_url("endpoint")
+        response = self.toggl.post_reports("endpoint", kwarg1=1, kwarg2="two")
+
+        response.raise_for_status.assert_called_once()
+
+        mock_requests.post.assert_called_once_with(url, json=dict(kwarg1=1, kwarg2="two"), timeout=self.toggl.timeout)
+
+    def test_toggl_detailed_time_entries(self, toggl_mock_post_reports):
+        dt = datetime(2024, 9, 25)
+        toggl_mock_post_reports.detailed_time_entries(dt, dt, kwarg1=1, kwarg2="two")
+
+        toggl_mock_post_reports.post_reports.assert_called_once_with(
+            "search/time_entries.csv",
+            start_date="2024-09-25",
+            end_date="2024-09-25",
+            rounding=1,
+            rounding_minutes=15,
+            kwarg1=1,
+            kwarg2="two",
+        )
+
+    def test_toggl_detailed_time_entries_dynamic_timeout(self, mock_requests):
+        # range of 6 months
+        # timeout should be 6 * 5 = 30
+        start = datetime(2024, 1, 1)
+        end = datetime(2024, 6, 30)
+        self.toggl.detailed_time_entries(start, end)
+
+        mock_requests.post.assert_called_once()
+        assert mock_requests.post.call_args.kwargs["timeout"] == 30
 
 
-def test_toggl_init(toggl):
-    token64 = "dG9rZW46YXBpX3Rva2Vu"
+class TestTogglWorkspace:
+    @pytest.fixture(autouse=True)
+    def setup(self, mock_requests):
+        self.toggl = TogglWorkspace("token", 1234)
 
-    assert toggl._token == "token"
-    assert toggl.workspace_id == 1234
-    assert toggl.workspace_url_fragment == "workspace/1234"
+    def test_api_url_resource(self):
+        assert self.toggl.api_url_resource == "workspaces/1234"
 
-    assert toggl.headers["Content-Type"] == "application/json"
+    def test_update_preferences(self, mocker, mock_requests):
+        url = "http://fake.url"
+        mocker.patch.object(self.toggl, "make_api_url", return_value=url)
+        prefs = {"pref1": "value1", "pref2": True}
 
-    user_agent = toggl.headers["User-Agent"]
-    assert "compilerla/compiler-admin" in user_agent
-    assert __version__ in user_agent
+        response = self.toggl.update_preferences(**prefs)
 
-    assert toggl.headers["Authorization"] == f"Basic {token64}"
-
-    assert toggl.timeout == 5
-
-
-def test_toggl_make_api_url(toggl):
-    url = toggl._make_api_url("endpoint")
-
-    assert url.startswith(toggl.API_BASE_URL)
-    assert toggl.API_VERSION_URL in url
-    assert toggl.workspaces_url_fragment in url
-    assert "/endpoint" in url
-
-
-def test_toggl_make_report_url(toggl):
-    url = toggl._make_report_url("endpoint")
-
-    assert url.startswith(toggl.API_BASE_URL)
-    assert toggl.API_REPORTS_BASE_URL in url
-    assert toggl.workspace_url_fragment in url
-    assert "/endpoint" in url
-
-
-def test_toggl_post_reports(mock_requests, toggl):
-    url = toggl._make_report_url("endpoint")
-    response = toggl.post_reports("endpoint", kwarg1=1, kwarg2="two")
-
-    response.raise_for_status.assert_called_once()
-
-    mock_requests.post.assert_called_once_with(url, json=dict(kwarg1=1, kwarg2="two"), timeout=toggl.timeout)
-
-
-def test_toggl_detailed_time_entries(toggl_mock_post_reports):
-    dt = datetime(2024, 9, 25)
-    toggl_mock_post_reports.detailed_time_entries(dt, dt, kwarg1=1, kwarg2="two")
-
-    toggl_mock_post_reports.post_reports.assert_called_once_with(
-        "search/time_entries.csv",
-        start_date="2024-09-25",
-        end_date="2024-09-25",
-        rounding=1,
-        rounding_minutes=15,
-        kwarg1=1,
-        kwarg2="two",
-    )
-
-
-def test_toggl_detailed_time_entries_dynamic_timeout(mock_requests, toggl):
-    # range of 6 months
-    # timeout should be 6 * 5 = 30
-    start = datetime(2024, 1, 1)
-    end = datetime(2024, 6, 30)
-    toggl.detailed_time_entries(start, end)
-
-    mock_requests.post.assert_called_once()
-    assert mock_requests.post.call_args.kwargs["timeout"] == 30
-
-
-def test_toggl_update_workspace_preferences(mock_requests, toggl, mocker):
-    url = "http://fake.url"
-    mocker.patch.object(toggl, "_make_api_url", return_value=url)
-    prefs = {"pref1": "value1", "pref2": True}
-    response = toggl.update_workspace_preferences(**prefs)
-
-    response.raise_for_status.assert_called_once()
-    toggl._make_api_url.assert_called_once_with("preferences")
-    mock_requests.post.assert_called_once_with(url, json=prefs, timeout=toggl.timeout)
+        response.raise_for_status.assert_called_once()
+        self.toggl.make_api_url.assert_called_once_with("preferences")
+        mock_requests.post.assert_called_once_with(url, json=prefs, timeout=self.toggl.timeout)
