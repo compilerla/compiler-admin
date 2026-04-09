9ea0ad117faee563004c11d7b1cb2da899d7b9fe
Kegan Maher <kegan@compiler.la>
2026-04-09 22:49:09 +0000

    feat(commands/user): filter to list inactive users

--- compiler_admin/commands/user/ls.py
+++ compiler_admin/commands/user/ls.py
@@ -1,34 +1,45 @@
 import click
 
-from compiler_admin.services import files
 from compiler_admin.services.google import CallGAMCommand
 from compiler_admin.services.toggl import TogglUsers
 
 
-def ls_google(**kwargs):
+def ls_google(inactive: bool = False, **kwargs):
     """Use GAM to print the users in the Google Workspace."""
-    command = ("print", "users")
+    flag = str(inactive).lower()
+    command = ("print", "users") + ("issuspended", flag, "isarchived", flag)
+
     CallGAMCommand(command)
 
 
-def ls_toggl(**kwargs):
-    """Use the Toggl API to get a list of Organization users."""
+def ls_toggl(inactive: bool = False, **kwargs):
+    """Use the Toggl API to get a list of users."""
     toggl = TogglUsers()
+    if inactive:
+        active_status = "inactive,invited"
+    else:
+        active_status = "active"
 
-    users = toggl.get_organization_users()
-    files.write_json(click.get_text_stream("stdout"), users)
+    users = toggl.get_organization_users(active_status=active_status)
+
+    # Mirroing the default GAM output for print users
+    click.echo(f"Got {len(users)} Users")
+    click.echo("email")
+    for user in users:
+        click.echo(user["email"])
 
 
 USER_SYSTEMS = {"google": ls_google, "toggl": ls_toggl}
 
 
 @click.command()
+@click.option("--inactive", help="List inactive users in the system.", is_flag=True)
 @click.argument(
     "system",
     type=click.Choice(USER_SYSTEMS.keys(), case_sensitive=False),
     default="google",
 )
-def ls(system: str, **kwargs):
-    """List users in the Compiler workspace SYSTEM (Google by default)."""
+def ls(system: str, inactive: bool, **kwargs):
+    """List users in the Compiler workspace."""
     ls_system = USER_SYSTEMS.get(system)
-    ls_system(**kwargs)
+    ls_system(inactive=inactive, **kwargs)
--- tests/commands/user/test_ls.py
+++ tests/commands/user/test_ls.py
@@ -12,7 +12,7 @@ def mock_google_CallGAMCommand(mock_google_CallGAMCommand):
 @pytest.fixture
 def mock_toggl_api(mocker):
     mock = mocker.patch(f"{MODULE}.TogglUsers").return_value
-    mock.get_organization_users.return_value = {"user": "name"}
+    mock.get_organization_users.return_value = [{"email": "user@example.com"}]
     return mock
 
 
@@ -30,12 +30,12 @@ def mock_ls_toggl(mocker, monkeypatch):
     return mock
 
 
-def test_ls__system_default(cli_runner, mock_ls_google, mock_ls_toggl):
+def test_ls_default(cli_runner, mock_ls_google, mock_ls_toggl):
     args = []
     result = cli_runner.invoke(ls, args)
 
     assert result.exit_code == RESULT_SUCCESS
-    mock_ls_google.assert_called_once()
+    mock_ls_google.assert_called_once_with(inactive=False)
     mock_ls_toggl.assert_not_called()
 
 
@@ -64,6 +64,22 @@ def test_ls__system_unknown(cli_runner, mock_ls_google, mock_ls_toggl):
     assert result.exit_code != RESULT_SUCCESS
     mock_ls_google.assert_not_called()
     mock_ls_toggl.assert_not_called()
+
+
+def test_ls__inactive(cli_runner, mock_ls_google):
+    args = ["--inactive"]
+    result = cli_runner.invoke(ls, args)
+
+    assert result.exit_code == RESULT_SUCCESS
+    mock_ls_google.assert_called_once_with(inactive=True)
+
+
+def test_ls__inactive__system_toggl(cli_runner, mock_ls_toggl):
+    args = ["--inactive", "toggl"]
+    result = cli_runner.invoke(ls, args)
+
+    assert result.exit_code == RESULT_SUCCESS
+    mock_ls_toggl.assert_called_once_with(inactive=True)
 
 
 def test_ls_google(mock_google_CallGAMCommand):
