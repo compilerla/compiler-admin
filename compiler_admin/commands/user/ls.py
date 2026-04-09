50edd53ce02ce2f5ce5f2bc1e60e1b1eb0bcd829
Kegan Maher <kegan@compiler.la>
2026-04-09 16:08:32 +0000

    feat(commands/user): ls takes system option
    defaults to google

--- compiler_admin/commands/user/ls.py
+++ compiler_admin/commands/user/ls.py
@@ -1,14 +1,28 @@
 import click
 
-from compiler_admin import RESULT_SUCCESS
 from compiler_admin.services.google import CallGAMCommand
+
+USER_SYSTEMS = ["google"]
 
 
 @click.command()
-def ls(**kwargs):
+@click.option(
+    "--system",
+    help="The system from which to list accounts.",
+    type=click.Choice(USER_SYSTEMS, case_sensitive=False),
+    default="google",
+)
+def ls(system: str, **kwargs):
     """Lists users in the Compiler workspace."""
-    click.echo("Listing users in Compiler workspace")
+    match system:
+        case "google":
+            ls_google(**kwargs)
+        case _:
+            click.echo(f"Unknown user system: {system}")
+
+
+def ls_google(**kwargs):
+    click.echo("Listing users in Compiler Google workspace")
 
     command = ("print", "users")
     CallGAMCommand(command)
-    raise SystemExit(RESULT_SUCCESS)
--- tests/commands/user/test_ls.py
+++ tests/commands/user/test_ls.py
@@ -1,7 +1,7 @@
 import pytest
 
 from compiler_admin import RESULT_SUCCESS
-from compiler_admin.commands.user.ls import __name__ as MODULE, ls
+from compiler_admin.commands.user.ls import __name__ as MODULE, ls, ls_google
 
 
 @pytest.fixture
@@ -9,9 +9,36 @@ def mock_google_CallGAMCommand(mock_google_CallGAMCommand):
     return mock_google_CallGAMCommand(MODULE)
 
 
-def test_ls(cli_runner, mock_google_CallGAMCommand):
+@pytest.fixture
+def mock_ls_google(mocker):
+    return mocker.patch(f"{MODULE}.ls_google")
+
+
+def test_ls__system_default(cli_runner, mock_ls_google):
     args = []
     result = cli_runner.invoke(ls, args)
 
     assert result.exit_code == RESULT_SUCCESS
+    mock_ls_google.assert_called_once()
+
+
+def test_ls__system_google(cli_runner, mock_ls_google):
+    args = ["--system", "google"]
+    result = cli_runner.invoke(ls, args)
+
+    assert result.exit_code == RESULT_SUCCESS
+    mock_ls_google.assert_called_once()
+
+
+def test_ls__system_unknown(cli_runner, mock_ls_google):
+    args = ["--system", "unknown"]
+    result = cli_runner.invoke(ls, args)
+
+    assert result.exit_code != RESULT_SUCCESS
+    mock_ls_google.assert_not_called()
+
+
+def test_ls_google(mock_google_CallGAMCommand):
+    ls_google()
+
     mock_google_CallGAMCommand.assert_called_once()
