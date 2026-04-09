2d5bdb8d1c3706334bdeceba20f23f7800d593db
Kegan Maher <kegan@compiler.la>
2026-04-09 16:59:07 +0000

    feat(commands/user): ls supports google and toggl systems

--- compiler_admin/commands/user/ls.py
+++ compiler_admin/commands/user/ls.py
@@ -1,28 +1,34 @@
 import click
 
+from compiler_admin.services import files
 from compiler_admin.services.google import CallGAMCommand
-
-USER_SYSTEMS = ["google"]
-
-
-@click.command()
-@click.option(
-    "--system",
-    help="The system from which to list accounts.",
-    type=click.Choice(USER_SYSTEMS, case_sensitive=False),
-    default="google",
-)
-def ls(system: str, **kwargs):
-    """Lists users in the Compiler workspace."""
-    match system:
-        case "google":
-            ls_google(**kwargs)
-        case _:
-            click.echo(f"Unknown user system: {system}")
+from compiler_admin.services.toggl import TogglUsers
 
 
 def ls_google(**kwargs):
-    click.echo("Listing users in Compiler Google workspace")
-
+    """Use GAM to print the users in the Google Workspace."""
     command = ("print", "users")
     CallGAMCommand(command)
+
+
+def ls_toggl(**kwargs):
+    """Use the Toggl API to get a list of Organization users."""
+    toggl = TogglUsers()
+
+    users = toggl.get_organization_users()
+    files.write_json(click.get_text_stream("stdout"), users)
+
+
+USER_SYSTEMS = {"google": ls_google, "toggl": ls_toggl}
+
+
+@click.command()
+@click.argument(
+    "system",
+    type=click.Choice(USER_SYSTEMS.keys(), case_sensitive=False),
+    default="google",
+)
+def ls(system: str, **kwargs):
+    """List users in the Compiler workspace SYSTEM (Google by default)."""
+    ls_system = USER_SYSTEMS.get(system)
+    ls_system(**kwargs)
--- tests/commands/user/test_ls.py
+++ tests/commands/user/test_ls.py
@@ -1,7 +1,7 @@
 import pytest
 
 from compiler_admin import RESULT_SUCCESS
-from compiler_admin.commands.user.ls import __name__ as MODULE, ls, ls_google
+from compiler_admin.commands.user.ls import USER_SYSTEMS, __name__ as MODULE, ls, ls_google, ls_toggl
 
 
 @pytest.fixture
@@ -10,35 +10,69 @@ def mock_google_CallGAMCommand(mock_google_CallGAMCommand):
 
 
 @pytest.fixture
-def mock_ls_google(mocker):
-    return mocker.patch(f"{MODULE}.ls_google")
+def mock_toggl_api(mocker):
+    mock = mocker.patch(f"{MODULE}.TogglUsers").return_value
+    mock.get_organization_users.return_value = {"user": "name"}
+    return mock
 
 
-def test_ls__system_default(cli_runner, mock_ls_google):
+@pytest.fixture
+def mock_ls_google(mocker, monkeypatch):
+    mock = mocker.patch(f"{MODULE}.ls_google")
+    monkeypatch.setitem(USER_SYSTEMS, "google", mock)
+    return mock
+
+
+@pytest.fixture
+def mock_ls_toggl(mocker, monkeypatch):
+    mock = mocker.patch(f"{MODULE}.ls_toggl")
+    monkeypatch.setitem(USER_SYSTEMS, "toggl", mock)
+    return mock
+
+
+def test_ls__system_default(cli_runner, mock_ls_google, mock_ls_toggl):
     args = []
     result = cli_runner.invoke(ls, args)
 
     assert result.exit_code == RESULT_SUCCESS
     mock_ls_google.assert_called_once()
+    mock_ls_toggl.assert_not_called()
 
 
-def test_ls__system_google(cli_runner, mock_ls_google):
-    args = ["--system", "google"]
+def test_ls__system_google(cli_runner, mock_ls_google, mock_ls_toggl):
+    args = ["google"]
     result = cli_runner.invoke(ls, args)
 
     assert result.exit_code == RESULT_SUCCESS
     mock_ls_google.assert_called_once()
+    mock_ls_toggl.assert_not_called()
 
 
-def test_ls__system_unknown(cli_runner, mock_ls_google):
-    args = ["--system", "unknown"]
+def test_ls__system_toggl(cli_runner, mock_ls_google, mock_ls_toggl):
+    args = ["toggl"]
+    result = cli_runner.invoke(ls, args)
+
+    assert result.exit_code == RESULT_SUCCESS
+    mock_ls_google.assert_not_called()
+    mock_ls_toggl.assert_called_once()
+
+
+def test_ls__system_unknown(cli_runner, mock_ls_google, mock_ls_toggl):
+    args = ["unknown"]
     result = cli_runner.invoke(ls, args)
 
     assert result.exit_code != RESULT_SUCCESS
     mock_ls_google.assert_not_called()
+    mock_ls_toggl.assert_not_called()
 
 
 def test_ls_google(mock_google_CallGAMCommand):
     ls_google()
 
     mock_google_CallGAMCommand.assert_called_once()
+
+
+def test_ls_toggl(mock_toggl_api):
+    ls_toggl()
+
+    mock_toggl_api.get_organization_users.assert_called_once()
