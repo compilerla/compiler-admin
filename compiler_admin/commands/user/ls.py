8a2028e4bbb1f9e5eb5fc941ec21ef97ea9f22e8
Kegan Maher <kegan@compiler.la>
2026-04-10 19:16:26 +0000

    feat(commands/user): flag for list output format

--- compiler_admin/commands/user/ls.py
+++ compiler_admin/commands/user/ls.py
@@ -1,38 +1,82 @@
 import click
+import pandas as pd
 
+from compiler_admin import Format
+from compiler_admin.services import files
 from compiler_admin.services.google import get_users
 from compiler_admin.services.toggl import TogglUsers
 
 
-def ls_google(inactive: bool = False, **kwargs):
+def ls_google(inactive: bool = False, format: int = Format.BASIC, **kwargs):
     """Use GAM to print the users in the Google Workspace."""
-    output = get_users(inactive, **kwargs)
+    output = get_users(inactive=inactive, format=format, **kwargs)
     click.echo(output)
 
 
-def ls_toggl(inactive: bool = False, **kwargs):
+def ls_toggl(inactive: bool = False, format: int = Format.BASIC, **kwargs):
     """Use the Toggl API to get a list of users."""
     toggl = TogglUsers()
     users = toggl.get_organization_users(inactive=inactive, **kwargs)
+    users_df = pd.DataFrame(users)
 
-    # Mirroing the default GAM output for print users
-    click.echo(f"Got {len(users)} Users")
-    click.echo("email")
-    for user in users:
-        click.echo(user["email"])
+    # Mirroing the GAM output style
+    stdout = click.get_text_stream("stdout")
+
+    match format:
+        case Format.BASIC:
+            click.echo(f"Got {len(users)} Users")
+            files.write_csv(stdout, users_df, columns=["email"])
+        case Format.CSV:
+            click.echo(f"Got {len(users)} Users")
+            files.write_csv(
+                stdout,
+                users_df,
+                columns=[
+                    "email",
+                    "name",
+                    "id",
+                    "user_id",
+                    "role_id",
+                    "organization_id",
+                    "inactive",
+                    "joined",
+                    "can_edit_email",
+                    "2fa_enabled",
+                    "avatar_url",
+                ],
+            )
+        case Format.JSON:
+            files.write_json(stdout, users)
 
 
 USER_SYSTEMS = {"google": ls_google, "toggl": ls_toggl}
+FORMATS = {
+    "b": Format.BASIC,
+    "basic": Format.BASIC,
+    "c": Format.CSV,
+    "csv": Format.CSV,
+    "j": Format.JSON,
+    "json": Format.JSON,
+}
 
 
 @click.command()
 @click.option("--inactive", help="List inactive users in the system.", is_flag=True)
+@click.option(
+    "--format",
+    "format_key",
+    help="The format of the output.",
+    type=click.Choice(FORMATS.keys(), case_sensitive=False),
+    default="basic",
+)
 @click.argument(
     "system",
     type=click.Choice(USER_SYSTEMS.keys(), case_sensitive=False),
     default="google",
 )
-def ls(system: str, inactive: bool, **kwargs):
+def ls(system: str, inactive: bool, format_key: str, **kwargs):
     """List users in the Compiler workspace."""
     ls_system = USER_SYSTEMS.get(system)
-    ls_system(inactive=inactive, **kwargs)
+    format = FORMATS.get(format_key)
+
+    ls_system(inactive=inactive, format=format, **kwargs)
--- tests/commands/user/test_ls.py
+++ tests/commands/user/test_ls.py
@@ -1,7 +1,9 @@
+import json
+
 import pytest
 
-from compiler_admin import Result
-from compiler_admin.commands.user.ls import USER_SYSTEMS, __name__ as MODULE, ls, ls_google, ls_toggl
+from compiler_admin import Format, Result
+from compiler_admin.commands.user.ls import FORMATS, USER_SYSTEMS, __name__ as MODULE, ls, ls_google, ls_toggl
 
 
 @pytest.fixture
@@ -35,7 +37,7 @@ def test_ls_default(cli_runner, mock_ls_google, mock_ls_toggl):
     result = cli_runner.invoke(ls, args)
 
     assert result.exit_code == Result.SUCCESS
-    mock_ls_google.assert_called_once_with(inactive=False)
+    mock_ls_google.assert_called_once_with(inactive=False, format=Format.BASIC)
     mock_ls_toggl.assert_not_called()
 
 
@@ -71,7 +73,7 @@ def test_ls__inactive(cli_runner, mock_ls_google):
     result = cli_runner.invoke(ls, args)
 
     assert result.exit_code == Result.SUCCESS
-    mock_ls_google.assert_called_once_with(inactive=True)
+    mock_ls_google.assert_called_once_with(inactive=True, format=Format.BASIC)
 
 
 def test_ls__inactive__system_toggl(cli_runner, mock_ls_toggl):
@@ -79,13 +81,20 @@ def test_ls__inactive__system_toggl(cli_runner, mock_ls_toggl):
     result = cli_runner.invoke(ls, args)
 
     assert result.exit_code == Result.SUCCESS
-    mock_ls_toggl.assert_called_once_with(inactive=True)
+    mock_ls_toggl.assert_called_once_with(inactive=True, format=Format.BASIC)
 
 
 def test_ls_google(mock_google_get_users):
     ls_google()
 
     mock_google_get_users.assert_called_once()
+
+
+@pytest.mark.parametrize("format", set(FORMATS.values()))
+def test_ls_google__format(mock_google_get_users, format):
+    ls_google(format=format)
+
+    mock_google_get_users.assert_called_once_with(inactive=False, format=format)
 
 
 def test_ls_toggl__default(mock_toggl_api):
@@ -98,3 +107,79 @@ def test_ls_toggl__inactive(mock_toggl_api):
     ls_toggl(inactive=True)
 
     mock_toggl_api.get_organization_users.assert_called_once_with(inactive=True)
+
+
+MOCK_USERS = [
+    {
+        "email": "email1@example.com",
+        "name": "name1",
+        "id": "id1",
+        "user_id": "user1",
+        "role_id": "role1",
+        "organization_id": 5678,
+        "inactive": False,
+        "joined": True,
+        "can_edit_email": False,
+        "2fa_enabled": True,
+        "avatar_url": "https://example.com/users/1",
+        "extra": "extra1",
+    },
+    {
+        "email": "email2@example.com",
+        "name": "name2",
+        "id": "id2",
+        "user_id": "user2",
+        "role_id": "role2",
+        "organization_id": 5678,
+        "inactive": False,
+        "joined": True,
+        "can_edit_email": False,
+        "2fa_enabled": True,
+        "avatar_url": "https://example.com/users/2",
+        "extra": "extra1",
+    },
+]
+
+
+@pytest.mark.parametrize(
+    "format,expected_out,not_expected_out",
+    [
+        (Format.BASIC, ["Got 2 Users", "email\n", "email1", "email2"], ["name1", "name2"]),
+        (
+            Format.CSV,
+            [
+                "Got 2 Users",
+                "email,name,id,user_id,role_id,organization_id,inactive,joined,can_edit_email,2fa_enabled,avatar_url\n",
+                "email1",
+                "email2",
+                "name1",
+                "name2",
+                "id1",
+                "id2",
+                "user1",
+                "user2",
+                "role1",
+                "role2",
+                "5678",
+                "https://example.com/users/1",
+                "https://example.com/users/2",
+            ],
+            ["extra1", "extra2"],
+        ),
+        (
+            Format.JSON,
+            [json.dumps(MOCK_USERS, indent=2)],
+            [],
+        ),
+    ],
+)
+def test_ls__format(mock_toggl_api, capfd, format, expected_out, not_expected_out):
+    mock_toggl_api.get_organization_users.return_value = MOCK_USERS
+
+    ls_toggl(format=format)
+    captured = capfd.readouterr()
+
+    for item in expected_out:
+        assert item in captured.out
+    for item in not_expected_out:
+        assert item not in captured.out
