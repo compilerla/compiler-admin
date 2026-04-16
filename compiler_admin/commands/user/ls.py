009c7764e95bb3dcbdfb1576c8340b9b9d2c93ea
Kegan Maher <kegan@compiler.la>
2026-04-16 15:50:16 +0000

    feat(commands/user): list groups or users

--- compiler_admin/commands/user/ls.py
+++ compiler_admin/commands/user/ls.py
@@ -3,50 +3,64 @@ import pandas as pd
 
 from compiler_admin import Format
 from compiler_admin.services import files
-from compiler_admin.services.google import get_users
+from compiler_admin.services.google import get_org_units, get_users
 from compiler_admin.services.toggl import TogglUsers
 
 
-def ls_google(inactive: bool = False, format: int = Format.BASIC, **kwargs):
+def ls_google(format: int = Format.BASIC, groups: bool = False, inactive: bool = False, **kwargs):
     """Use GAM to print the users in the Google Workspace."""
-    output = get_users(inactive=inactive, format=format, **kwargs)
-    click.echo(output)
+    if groups:
+        get_org_units(format=format)
+    else:
+        output = get_users(inactive=inactive, format=format, **kwargs)
+        click.echo(output)
 
 
-def ls_toggl(inactive: bool = False, format: int = Format.BASIC, **kwargs):
-    """Use the Toggl API to get a list of users."""
+def ls_toggl(format: int = Format.BASIC, groups: bool = False, inactive: bool = False, **kwargs):
+    """Use the Toggl API to get a list of users.
+
+    Mirrors the GAM output style for Google users.
+    """
     toggl = TogglUsers()
+
+    if groups:
+        click.echo("Getting all Toggl groups...", err=True)
+        groups = toggl.get_organization_groups()
+        groups_df = pd.DataFrame(groups)
+        click.echo(f"Got {len(groups)} Groups", err=True)
+
+        stdout = click.get_text_stream("stdout")
+        if format in [Format.BASIC, Format.CSV]:
+            files.write_csv(stdout, groups_df, columns=["group_id", "name", "at"])
+        elif format == Format.JSON:
+            files.write_json(stdout, groups)
+        return
+
+    click.echo("Getting all Toggl users...", err=True)
     users = toggl.get_organization_users(inactive=inactive, **kwargs)
     users_df = pd.DataFrame(users)
+    click.echo(f"Got {len(users)} Users", err=True)
 
-    # Mirroing the GAM output style
     stdout = click.get_text_stream("stdout")
-
-    match format:
-        case Format.BASIC:
-            click.echo(f"Got {len(users)} Users")
-            files.write_csv(stdout, users_df, columns=["email"])
-        case Format.CSV:
-            click.echo(f"Got {len(users)} Users")
-            files.write_csv(
-                stdout,
-                users_df,
-                columns=[
-                    "email",
-                    "name",
-                    "id",
-                    "user_id",
-                    "role_id",
-                    "organization_id",
-                    "inactive",
-                    "joined",
-                    "can_edit_email",
-                    "2fa_enabled",
-                    "avatar_url",
-                ],
-            )
-        case Format.JSON:
-            files.write_json(stdout, users)
+    columns = ["email"]
+    if format == Format.BASIC:
+        files.write_csv(stdout, users_df, columns=columns)
+    elif format == Format.CSV:
+        columns += [
+            "name",
+            "id",
+            "user_id",
+            "role_id",
+            "organization_id",
+            "inactive",
+            "joined",
+            "can_edit_email",
+            "2fa_enabled",
+            "avatar_url",
+        ]
+        files.write_csv(stdout, users_df, columns=columns)
+    elif format == Format.JSON:
+        files.write_json(stdout, users)
 
 
 USER_SYSTEMS = {"google": ls_google, "toggl": ls_toggl}
@@ -61,6 +75,7 @@ FORMATS = {
 
 
 @click.command()
+@click.option("--groups", is_flag=True, help="Show groups instead.")
 @click.option("--inactive", help="List inactive users in the system.", is_flag=True)
 @click.option(
     "--format",
@@ -74,9 +89,9 @@ FORMATS = {
     type=click.Choice(USER_SYSTEMS.keys(), case_sensitive=False),
     default="google",
 )
-def ls(system: str, inactive: bool, format_key: str, **kwargs):
+def ls(system: str, groups: bool, inactive: bool, format_key: str, **kwargs):
     """List users in the Compiler workspace."""
-    ls_system = USER_SYSTEMS.get(system)
     format = FORMATS.get(format_key)
 
-    ls_system(inactive=inactive, format=format, **kwargs)
+    ls_system = USER_SYSTEMS.get(system)
+    ls_system(format=format, groups=groups, inactive=inactive, **kwargs)
--- tests/commands/user/test_ls.py
+++ tests/commands/user/test_ls.py
@@ -5,6 +5,57 @@ import pytest
 from compiler_admin import Format, Result
 from compiler_admin.commands.user.ls import FORMATS, USER_SYSTEMS, __name__ as MODULE, ls, ls_google, ls_toggl
 
+MOCK_GROUPS = [
+    {
+        "group_id": "group1",
+        "name": "name1",
+        "at": "2026-04-20T20:20:00.000000Z",
+        "extra": "extra1",
+    },
+    {
+        "group_id": "group2",
+        "name": "name2",
+        "at": "2026-04-20T20:40:00.000000Z",
+        "extra": "extra2",
+    },
+]
+
+MOCK_USERS = [
+    {
+        "email": "email1@example.com",
+        "name": "name1",
+        "id": "id1",
+        "user_id": "user1",
+        "role_id": "role1",
+        "organization_id": 5678,
+        "inactive": False,
+        "joined": True,
+        "can_edit_email": False,
+        "2fa_enabled": True,
+        "avatar_url": "https://example.com/users/1",
+        "extra": "extra1",
+    },
+    {
+        "email": "email2@example.com",
+        "name": "name2",
+        "id": "id2",
+        "user_id": "user2",
+        "role_id": "role2",
+        "organization_id": 5678,
+        "inactive": False,
+        "joined": True,
+        "can_edit_email": False,
+        "2fa_enabled": True,
+        "avatar_url": "https://example.com/users/2",
+        "extra": "extra1",
+    },
+]
+
+
+@pytest.fixture
+def mock_google_get_org_units(mocker):
+    return mocker.patch(f"{MODULE}.get_org_units")
+
 
 @pytest.fixture
 def mock_google_get_users(mocker):
@@ -14,7 +65,8 @@ def mock_google_get_users(mocker):
 @pytest.fixture
 def mock_toggl_api(mocker):
     mock = mocker.patch(f"{MODULE}.TogglUsers").return_value
-    mock.get_organization_users.return_value = [{"email": "user@example.com"}]
+    mock.get_organization_users.return_value = MOCK_USERS
+    mock.get_organization_groups.return_value = MOCK_GROUPS
     return mock
 
 
@@ -37,8 +89,26 @@ def test_ls_default(cli_runner, mock_ls_google, mock_ls_toggl):
     result = cli_runner.invoke(ls, args)
 
     assert result.exit_code == Result.SUCCESS
-    mock_ls_google.assert_called_once_with(inactive=False, format=Format.BASIC)
+    mock_ls_google.assert_called_once_with(format=Format.BASIC, groups=False, inactive=False)
     mock_ls_toggl.assert_not_called()
+
+
+def test_ls__groups(cli_runner, mock_ls_google, mock_ls_toggl):
+    args = ["--groups"]
+    result = cli_runner.invoke(ls, args)
+
+    assert result.exit_code == Result.SUCCESS
+    mock_ls_google.assert_called_once_with(format=Format.BASIC, groups=True, inactive=False)
+    mock_ls_toggl.assert_not_called()
+
+
+def test_ls__groups__system_toggl(cli_runner, mock_ls_google, mock_ls_toggl):
+    args = ["--groups", "toggl"]
+    result = cli_runner.invoke(ls, args)
+
+    assert result.exit_code == Result.SUCCESS
+    mock_ls_google.assert_not_called()
+    mock_ls_toggl.assert_called_once_with(format=Format.BASIC, groups=True, inactive=False)
 
 
 def test_ls__system_google(cli_runner, mock_ls_google, mock_ls_toggl):
@@ -73,7 +143,7 @@ def test_ls__inactive(cli_runner, mock_ls_google):
     result = cli_runner.invoke(ls, args)
 
     assert result.exit_code == Result.SUCCESS
-    mock_ls_google.assert_called_once_with(inactive=True, format=Format.BASIC)
+    mock_ls_google.assert_called_once_with(format=Format.BASIC, groups=False, inactive=True)
 
 
 def test_ls__inactive__system_toggl(cli_runner, mock_ls_toggl):
@@ -81,7 +151,7 @@ def test_ls__inactive__system_toggl(cli_runner, mock_ls_toggl):
     result = cli_runner.invoke(ls, args)
 
     assert result.exit_code == Result.SUCCESS
-    mock_ls_toggl.assert_called_once_with(inactive=True, format=Format.BASIC)
+    mock_ls_toggl.assert_called_once_with(format=Format.BASIC, groups=False, inactive=True)
 
 
 def test_ls_google(mock_google_get_users):
@@ -97,10 +167,28 @@ def test_ls_google__format(mock_google_get_users, format):
     mock_google_get_users.assert_called_once_with(inactive=False, format=format)
 
 
-def test_ls_toggl__default(mock_toggl_api):
+def test_ls_google__groups(mock_google_get_org_units):
+    ls_google(groups=True)
+
+    mock_google_get_org_units.assert_called_once()
+
+
+def test_ls_toggl__default(mock_toggl_api, capfd):
     ls_toggl()
+    captured = capfd.readouterr()
 
     mock_toggl_api.get_organization_users.assert_called_once_with(inactive=False)
+    assert "Getting all Toggl users" in captured.err
+    assert "Got 2 Users" in captured.err
+
+
+def test_lg_toggl__groups(mock_toggl_api, capfd):
+    ls_toggl(groups=True)
+    captured = capfd.readouterr()
+
+    mock_toggl_api.get_organization_groups.assert_called_once_with()
+    assert "Getting all Toggl groups" in captured.err
+    assert "Got 2 Groups" in captured.err
 
 
 def test_ls_toggl__inactive(mock_toggl_api):
@@ -109,46 +197,18 @@ def test_ls_toggl__inactive(mock_toggl_api):
     mock_toggl_api.get_organization_users.assert_called_once_with(inactive=True)
 
 
-MOCK_USERS = [
-    {
-        "email": "email1@example.com",
-        "name": "name1",
-        "id": "id1",
-        "user_id": "user1",
-        "role_id": "role1",
-        "organization_id": 5678,
-        "inactive": False,
-        "joined": True,
-        "can_edit_email": False,
-        "2fa_enabled": True,
-        "avatar_url": "https://example.com/users/1",
-        "extra": "extra1",
-    },
-    {
-        "email": "email2@example.com",
-        "name": "name2",
-        "id": "id2",
-        "user_id": "user2",
-        "role_id": "role2",
-        "organization_id": 5678,
-        "inactive": False,
-        "joined": True,
-        "can_edit_email": False,
-        "2fa_enabled": True,
-        "avatar_url": "https://example.com/users/2",
-        "extra": "extra1",
-    },
-]
-
-
+@pytest.mark.usefixtures("mock_toggl_api")
 @pytest.mark.parametrize(
     "format,expected_out,not_expected_out",
     [
-        (Format.BASIC, ["Got 2 Users", "email\n", "email1", "email2"], ["name1", "name2"]),
+        (
+            Format.BASIC,
+            ["email\n", "email1", "email2"],
+            ["name,id,user_id,role_id,organization_id,inactive,joined,can_edit_email,2fa_enabled,avatar_url\n"],
+        ),
         (
             Format.CSV,
             [
-                "Got 2 Users",
                 "email,name,id,user_id,role_id,organization_id,inactive,joined,can_edit_email,2fa_enabled,avatar_url\n",
                 "email1",
                 "email2",
@@ -166,17 +226,56 @@ MOCK_USERS = [
             ],
             ["extra1", "extra2"],
         ),
-        (
-            Format.JSON,
-            [json.dumps(MOCK_USERS, indent=2)],
-            [],
-        ),
+        (Format.JSON, [json.dumps(MOCK_USERS, indent=2)], []),
+        (-1, [], []),
     ],
 )
-def test_ls__format(mock_toggl_api, capfd, format, expected_out, not_expected_out):
-    mock_toggl_api.get_organization_users.return_value = MOCK_USERS
-
+def test_ls_toggl__format(capfd, format, expected_out, not_expected_out):
     ls_toggl(format=format)
+    captured = capfd.readouterr()
+
+    for item in expected_out:
+        assert item in captured.out
+    for item in not_expected_out:
+        assert item not in captured.out
+
+
+@pytest.mark.usefixtures("mock_toggl_api")
+@pytest.mark.parametrize(
+    "format,expected_out,not_expected_out",
+    [
+        (
+            Format.BASIC,
+            [
+                "group_id,name,at\n",
+                "group1",
+                "group2",
+                "name1",
+                "name2",
+                "2026-04-20T20:20:00.000000Z",
+                "2026-04-20T20:40:00.000000Z",
+            ],
+            ["extra1", "extra2"],
+        ),
+        (
+            Format.CSV,
+            [
+                "group_id,name,at\n",
+                "group1",
+                "group2",
+                "name1",
+                "name2",
+                "2026-04-20T20:20:00.000000Z",
+                "2026-04-20T20:20:00.000000Z",
+            ],
+            ["extra1", "extra2"],
+        ),
+        (Format.JSON, [json.dumps(MOCK_GROUPS, indent=2)], []),
+        (-1, [], []),
+    ],
+)
+def test_ls_toggl__groups__format(capfd, format, expected_out, not_expected_out):
+    ls_toggl(format=format, groups=True)
     captured = capfd.readouterr()
 
     for item in expected_out:
