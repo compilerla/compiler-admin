36149dc9392fc7354e07c7a55b320aa69a4a3fcc
Kegan Maher <kegan@compiler.la>
2026-04-23 18:30:30 +0000

    refactor(commands/user/backupcodes): use Google service classes

--- compiler_admin/commands/user/backupcodes.py
+++ compiler_admin/commands/user/backupcodes.py
@@ -1,18 +1,18 @@
 import click
 
 from compiler_admin import Result
-from compiler_admin.services.google import get_backup_codes, user_account_name, user_exists
+from compiler_admin.services.google import GoogleAccount
 
 
 @click.command()
 @click.argument("username")
 def backupcodes(username: str, **kwargs):
     """Get backup codes for the user, creating a new set if needed."""
-    account = user_account_name(username)
+    account = GoogleAccount(username)
 
-    if not user_exists(account):
+    if not account.exists():
         click.echo(f"User does not exist: {account}")
         raise SystemExit(Result.FAILURE)
 
-    backup_codes = get_backup_codes(account)
+    backup_codes = account.get_backup_codes()
     click.echo(backup_codes)
