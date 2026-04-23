c63abc5e201f15be0b287798e820a38b51efd848
Kegan Maher <kegan@compiler.la>
2026-04-23 18:25:52 +0000

    refactor(commands/info): use Google service classes

--- tests/commands/test_info.py
+++ tests/commands/test_info.py
@@ -5,19 +5,15 @@ from compiler_admin.commands.info import __name__ as MODULE, info
 
 
 @pytest.fixture
-def mock_google_CallGAMCommand(mock_google_CallGAMCommand):
-    return mock_google_CallGAMCommand(MODULE)
+def mock_GoogleService(mocker):
+    return mocker.patch(f"{MODULE}.GoogleService").return_value
 
 
-@pytest.fixture
-def mock_google_CallGYBCommand(mock_google_CallGYBCommand):
-    return mock_google_CallGYBCommand(MODULE)
-
-
-def test_info(cli_runner, mock_google_CallGAMCommand, mock_google_CallGYBCommand):
+def test_info(cli_runner, mock_GoogleService):
     result = cli_runner.invoke(info)
 
     assert result.exit_code == Result.SUCCESS
     assert f"compiler-admin, version {__version__}" in result.output
-    assert mock_google_CallGAMCommand.call_count > 0
-    mock_google_CallGYBCommand.assert_called_once()
+
+    assert mock_GoogleService.gam_command.call_count == 2
+    mock_GoogleService.gyb_command.assert_called_once()
