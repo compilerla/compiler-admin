f5823ac1e6debd63184aaffa1f65916a36f5de80
Kegan Maher <kegan@compiler.la>
2026-04-23 18:32:25 +0000

    refactor(commands/user/restore): use Google service classes

--- compiler_admin/commands/user/restore.py
+++ compiler_admin/commands/user/restore.py
@@ -3,34 +3,22 @@ import pathlib
 import click
 
 from compiler_admin import Result
-from compiler_admin.services.google import USER_ARCHIVE, CallGYBCommand, user_account_name
+from compiler_admin.services.google import GoogleAccount, GoogleArchive, GoogleUsers
 
 
 @click.command()
 @click.argument("username")
 def restore(username: str):
     """Restore an email backup from a prior offboarding."""
-    account = user_account_name(username)
+    account = GoogleAccount(username)
     backup_dir = f"GYB-GMail-Backup-{account}"
 
     if not pathlib.Path(backup_dir).exists():
         click.echo(f"Couldn't find a local backup: {backup_dir}")
         raise SystemExit(Result.FAILURE)
 
-    click.echo(f"Found backup, starting restore process with dest: {USER_ARCHIVE} for account: {account}")
+    click.echo(f"Found backup, starting restore process with dest: {GoogleUsers.USER_ARCHIVE} for account: {account}")
 
-    CallGYBCommand(
-        (
-            "--service-account",
-            "--email",
-            USER_ARCHIVE,
-            "--action",
-            "restore",
-            "--local-folder",
-            backup_dir,
-            "--label-restored",
-            account,
-        )
-    )
+    GoogleArchive().restore_email_backup(account=account, backup_dir=backup_dir)
 
     click.echo(f"Email restore complete for: {account}")
